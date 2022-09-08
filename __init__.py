ec45355956e341c02a42e66ce2bc0565e960bf7f
Rik <gitaarik@posteo.net>
2022-09-08 12:48:04 +0200

    --wip-- [skip ci]

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 from mycroft import MycroftSkill, intent_file_handler
+# from pynput.mouse import Controller
+
+
+# mouse = Controller()
 
 
 class InputControl(MycroftSkill):
@@ -9,6 +13,8 @@ class InputControl(MycroftSkill):
     @intent_file_handler('control.input.intent')
     def handle_control_input(self, message):
         self.speak_dialog('control.input')
+        self.speak('Yo man')
+        self.speak(message)
 
 
 def create_skill():
